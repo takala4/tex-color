2ad7938f7c43a2aaed0a1e8e391e76fb582fc4d7
takala_lab_PC <takara.sakai.t1@dc.tohoku.ac.jp>
2018-11-05 14:04:24 +0900

    no message

--- spec/tex-color-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import TexColorView from '../lib/tex-color-view';
-
-describe('TexColorView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
